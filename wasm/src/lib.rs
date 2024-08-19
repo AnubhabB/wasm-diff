085f40aedbae685c5ea45f75d4db1b54fe5050f9
Anubhab Bandyopadhyay <4890833+AnubhabB@users.noreply.github.com>
2024-08-19 20:32:47 +0530

    Working demo

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -1,13 +1,5 @@
-use diffmatchpatch::{DiffMatchPatch, ToChars};
+use diff_match_patch_rs::dmp::DiffMatchPatch;
 use wasm_bindgen::prelude::*;
-
-#[wasm_bindgen]
-extern "C" {
-    // Use `js_namespace` here to bind `console.log(..)` instead of just
-    // `log(..)`
-    #[wasm_bindgen(js_namespace = console)]
-    fn log(s: &str);
-}
 
 #[wasm_bindgen]
 #[derive(Default)]
@@ -19,47 +11,29 @@ pub struct Differ {
 impl Differ {
     #[wasm_bindgen(constructor)]
     pub fn new() -> Self {
-        log("Initializing");
         console_error_panic_hook::set_once();
         
-        let dmp = DiffMatchPatch {
-            diff_timeout: None,
-            ..Default::default()
-        };
+        let dmp = DiffMatchPatch::default();
         Self {
             dmp
         }
     }
 
-    pub fn diff(&self, old: &str, new: &str) -> String {
-        log("Got data!");
-        let old = old.to_chars();
-        let new = new.to_chars();
+    pub fn diff(&self, old: &str, new: &str) -> Result<String, String> {
+        let diffs = match self.dmp.diff_main(old, new) {
+            Ok(d) => d,
+            Err(_) => {
+                return Err("error while diffing".to_string())
+            }
+        };
 
-        log("Got chars!");
+        let html = match DiffMatchPatch::diff_pretty_html(&diffs[..]) {
+            Ok(s) => s,
+            Err(_) => {
+                return Err("Error preparing HTML".to_string())
+            }
+        };
 
-        let mut diffs = self.dmp.diff_main(&old[..], &new[..], true);
-        log("Got diffs");
-        self.dmp.diff_cleanup_semantic(&mut diffs);
-
-        self.dmp.diff_to_html(&diffs[..])
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use wasm_bindgen_test::wasm_bindgen_test;
-
-    use super::Differ;
-
-    #[wasm_bindgen_test]
-    fn test_diff() {
-        let differ = Differ::new();
-        let old = "Let's start with some basics 😊. We've got your standard smiley face 🙂, your sad face ☹️, and your angry face 😠. But wait, there's more! 🤩 We've also got some more complex emotions like 😍, 🤤, and 🚀. And let's not forget about the classics: 😉, 👍, and 👏.";
-        let new = "Now, let's explore some emotional extremes 🌊. We've got your ecstatic face 🤩, your devastated face 😭, and your utterly confused face 🤯. But that's not all! 🤔 We've also got some subtle emotions like 😐, 🙃, and 👀.";
-
-        let diffs = differ.diff(old, new);
-        
-        println!("{diffs}");
+        Ok(html)
     }
 }
